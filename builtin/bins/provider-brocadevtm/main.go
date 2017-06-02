055f984e5f51f8e288524d07ecdf08c21148d1b4
Paul Warwick <paul.warwick@sky.uk>
2017-06-02 17:46:49 +0100

    provider/brocadevtm: Fixed bug with updating boolean values in Monitor

--- builtin/bins/provider-brocadevtm/main.go
+++ builtin/bins/provider-brocadevtm/main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
+	"github.com/hashicorp/terraform/builtin/providers/brocadevtm"
 	"github.com/hashicorp/terraform/plugin"
 	"github.com/hashicorp/terraform/terraform"
-	"github.com/hashicorp/terraform/builtin/providers/brocadevtm"
 )
 
 func main() {
